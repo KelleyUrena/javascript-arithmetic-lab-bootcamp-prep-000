75891a1248c902b82145f7ff8487d7e9ca44dee9
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-01 02:41:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,51 @@
+function add (a, b) {
+  return a + b
+}
+
+function subtract (a, b) {
+  return a - b
+}
+
+function multiply (a, b) {
+  return a * b
+}
+
+function divide (a, b) {
+  return a/b
+}
+
+function increments (n) {
+  n++
+  return n
+}
+
+function decrements(n){
+ n--
+ return n
+} 
+
+function makeInt(n) {
+  n = parseInt(n, 10)
+  return n
+}
+
+
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+describe('preserveDecimal(n)', function() {
+  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
+    expect(preserveDecimal('2.222')).toBe(2.222)
+  })
+
+  it('returns NaN as appropriate', function() {
+    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
+  })
+})
+
+
+
+
